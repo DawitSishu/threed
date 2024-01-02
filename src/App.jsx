03545aebbfd617f82c5862efda6f76a306c57f19
DawitSishu <dawitalemayhu55@gmail.com>
2024-01-02 08:33:19 -0500

    s

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ComputersCanvas from "./x";
+
+function App() {
+  return (
+    <>
+      <ComputersCanvas />
+    </>
+  );
+}
+
+export default App;
---
+++ src/x.jsx
@@ -0,0 +1,78 @@
+import React, { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
+
+const Computers = ({ isMobile }) => {
+  const computer = useGLTF("./some/pants.gltf");
+
+  return (
+    <mesh>
+      <hemisphereLight intensity={2.5} groundColor='black' />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <pointLight intensity={4} />
+      <primitive
+        object={computer.scene}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3.5, -2.2] : [0, -10.25, -1.5]}
+        rotation-y={0}
+        // rotation={[0, 0, 0]} // Adjust these values to change rotation
+      />
+    </mesh>
+  );
+};
+
+const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    // Set the initial value of the isMobile state variable
+    setIsMobile(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
+  return (
+    <Canvas
+      frameloop='demand'
+      shadows
+      dpr={[1, 2]}
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      
+      <OrbitControls
+          autoRotate
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers isMobile={isMobile} />
+
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default ComputersCanvas;
