a6c39b2d2771c924eb3aa54db90ddc754391d93b
DawitSishu <dawitalemayhu55@gmail.com>
2024-01-03 12:34:22 -0500

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,9 @@ import SleeveCanvas from "./sleeve";
 import ComputersCanvas from "./pants";
 import ShirtCanvas from "./Shirt";
 import SockCanvas from "./Sock";
+import WhiteSleeve from "./WhiteSleeve";
+import WhitePant from "./whitePant";
+import WhiteShort from "./WhiteShort";
 
 function App() {
   const [currentDetailsIndex, setCurrentDetailsIndex] = useState(0);
@@ -50,6 +53,27 @@ function App() {
       price: "BR6,882.00",
       deliveryTime: "DELIVERY WITHIN 4 WEEKS",
     },
+    {
+      brand: "sock6",
+      category: "BAG",
+      name: "Vss",
+      price: "BR6,882.00",
+      deliveryTime: "DELIVERY WITHIN 4 WEEKS",
+    },
+    {
+      brand: "sock6",
+      category: "BAG",
+      name: "Vss",
+      price: "BR6,882.00",
+      deliveryTime: "DELIVERY WITHIN 4 WEEKS",
+    },
+    {
+      brand: "sock6",
+      category: "BAG",
+      name: "Vss",
+      price: "BR6,882.00",
+      deliveryTime: "DELIVERY WITHIN 4 WEEKS",
+    },
   ];
 
   const detailsRef = useRef(null);
@@ -72,19 +96,19 @@ function App() {
         onEnterBack: () => setCurrentDetailsIndex(index),
       });
     });
-    panels.forEach((panel, index) => {
-      ScrollTrigger.create({
-        trigger: panel,
-        start: "top top", // Align the start of the panel with the top of the viewport
-        end: () => `+=${panel.offsetHeight}`, // Set the end based on panel height
-        snap: {
-          snapTo: 1, // Snap to one panel at a time
-          duration: { min: 0.1, max: 0.2 }, // Set minimum and maximum duration for snapping
-          delay: 0.1, // Delay before snapping
-          ease: "power1.inOut", // Easing function for snapping animation
-        },
-      });
-    });
+    // panels.forEach((panel, index) => {
+    //   ScrollTrigger.create({
+    //     trigger: panel,
+    //     start: "top top", // Align the start of the panel with the top of the viewport
+    //     end: () => `+=${panel.offsetHeight}`, // Set the end based on panel height
+    //     snap: {
+    //       snapTo: 1, // Snap to one panel at a time
+    //       duration: { min: 0.1, max: 0.2 }, // Set minimum and maximum duration for snapping
+    //       delay: 0.1, // Delay before snapping
+    //       // ease: "power1.inOut", // Easing function for snapping animation
+    //     },
+    //   });
+    // });
     const timer = setTimeout(() => {
       setLoading(false); // Set isLoading to false after 5 seconds
     }, 4000);
@@ -94,19 +118,19 @@ function App() {
   useEffect(() => {
     if (isLoading) {
       // While loading, prevent scrolling on the body
-      document.body.style.overflowX = 'hidden';
-      document.body.style.overflowY = 'hidden';
+      document.body.style.overflowX = "hidden";
+      document.body.style.overflowY = "hidden";
     } else {
       // Once loading is done, allow scrolling on the body
-      document.body.style.overflowX = 'hidden';
-      document.body.style.overflowY = 'scroll';
+      document.body.style.overflowX = "hidden";
+      document.body.style.overflowY = "scroll";
     }
   }, [isLoading]);
 
   return (
     <div>
       {isLoading ? (
-        <div style={{width:"100vw", height:"100vh"}}>
+        <div style={{ width: "100vw", height: "100vh" }}>
           <img src={gif} alt="Loading..." />
           <a href="#">SPLY</a>
         </div>
@@ -126,18 +150,27 @@ function App() {
       <div className="panel">
         <SockCanvas />
       </div>
-
+      <div className="panel">
+        <WhitePant />
+      </div>
       <div className="panel">
         <ComputersCanvas />
       </div>
+
       <div className="panel">
-        <ShirtCanvas />
+        <SleeveCanvas />
+      </div>
+      <div className="panel">
+        <WhiteSleeve />
       </div>
       <div className="panel">
         <ShortCanvas />
       </div>
       <div className="panel">
-        <SleeveCanvas />
+        <WhiteShort />
+      </div>
+      <div className="panel">
+        <ShirtCanvas />
       </div>
     </div>
   );
---
+++ src/WhiteShort.jsx
@@ -0,0 +1,110 @@
+import React, { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import "./App.css";
+import styled from "styled-components";
+
+const StyledCanvas = styled(Canvas)`
+  /* Add your custom styles here */
+
+  /* Media query */
+  @media (max-width: 507px) {
+    width: 60vw !important;
+    height: 60vh !important;
+  }
+`;
+
+const Short = ({ isMobile }) => {
+  const computer = useGLTF("./some/Vulture short GLB file INVERTED COLOR .glb");
+
+  return (
+    <mesh>
+      <hemisphereLight intensity={2.5} groundColor="black" />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={0}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <primitive
+        object={computer.scene}
+        scale={isMobile ? 0.4 : 0.4}
+        position={isMobile ? [0, 0, 0] : [0, 0, 0]}
+        rotation={[0, 3, 0]}
+      />
+    </mesh>
+  );
+};
+
+const WhiteShort = () => {
+  const [isMobile, setIsMobile] = useState(false);
+  const [details, setDetails] = useState({
+    brand: "SPLY",
+    category: "BAG(1)",
+    name: "YZY PODS",
+    price: "BR11, 470.00",
+    deliveryTime: "DELIVERY WITHIN 4 WEEKS",
+  });
+
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    // Set the initial value of the isMobile state variable
+    setIsMobile(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
+  return (
+    <>
+      <div className="container">
+        {/* <div className="details">
+          <div className="det"> 
+            <a href="#">{details.brand}</a>
+            <a href="#">{details.category}</a>
+          </div>
+          <div className="det">
+            <a href="#">{details.name}</a>
+            <a href="#">{details.price}</a>
+          </div>
+
+          <p className="paragraph">{details.deliveryTime}</p>
+        </div> */}
+
+        <StyledCanvas
+          frameloop="demand"
+          shadows
+          dpr={[1, 2]}
+          camera={{ position: [20, 3, 5], fov: 25 }}
+          gl={{ preserveDrawingBuffer: true }}
+        >
+          <OrbitControls
+            // autoRotate
+            enableZoom={false}
+            maxPolarAngle={Math.PI / 2}
+            minPolarAngle={Math.PI / 2}
+          />
+          <Short isMobile={isMobile} />
+
+          <Preload all />
+        </StyledCanvas>
+      </div>
+    </>
+  );
+};
+
+export default WhiteShort;
