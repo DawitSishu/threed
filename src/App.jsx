0ef24f59289e2acaf59c89ced076147302d54c80
DawitSishu <dawitalemayhu55@gmail.com>
2024-01-03 13:57:02 -0500

    sth

--- src/App.jsx
+++ src/App.jsx
@@ -12,69 +12,65 @@ import SockCanvas from "./Sock";
 import WhiteSleeve from "./WhiteSleeve";
 import WhitePant from "./whitePant";
 import WhiteShort from "./WhiteShort";
+const detail = [
+  {
+    brand: "sock1",
+    category: "BAG",
+    name: "Vss",
+    price: "BR6,882.00",
+    deliveryTime: "DELIVERY WITHIN 4 WEEKS",
+    iswhite: false,
+  },
+  {
+    brand: "sock2",
+    category: "BAG",
+    name: "Vss",
+    price: "BR6,882.00",
+    deliveryTime: "DELIVERY WITHIN 4 WEEKS",
+    iswhite: false,
+  },
+  {
+    brand: "sock3",
+    category: "BAG",
+    name: "Vss",
+    price: "BR6,882.00",
+    deliveryTime: "DELIVERY WITHIN 4 WEEKS",
+    iswhite: false,
+  },
+  {
+    brand: "sock4",
+    category: "BAG",
+    name: "Vss",
+    price: "BR6,882.00",
+    deliveryTime: "DELIVERY WITHIN 4 WEEKS",
+    iswhite: false,
+  },
+  {
+    brand: "sock5",
+    category: "BAG",
+    name: "Vss",
+    price: "BR6,882.00",
+    deliveryTime: "DELIVERY WITHIN 4 WEEKS",
+  },
+];
 
 function App() {
   const [currentDetailsIndex, setCurrentDetailsIndex] = useState(0);
   const [isLoading, setLoading] = useState(true);
+  const [details, setDetails] = useState(detail);
+  const [canvasIndex, setCanvasIndex] = useState(-1);
 
-  const details = [
-    {
-      brand: "sock1",
-      category: "BAG",
-      name: "Vss",
-      price: "BR6,882.00",
-      deliveryTime: "DELIVERY WITHIN 4 WEEKS",
-    },
-    {
-      brand: "sock2",
-      category: "BAG",
-      name: "Vss",
-      price: "BR6,882.00",
-      deliveryTime: "DELIVERY WITHIN 4 WEEKS",
-    },
-    {
-      brand: "sock3",
-      category: "BAG",
-      name: "Vss",
-      price: "BR6,882.00",
-      deliveryTime: "DELIVERY WITHIN 4 WEEKS",
-    },
-    {
-      brand: "sock4",
-      category: "BAG",
-      name: "Vss",
-      price: "BR6,882.00",
-      deliveryTime: "DELIVERY WITHIN 4 WEEKS",
-    },
-    {
-      brand: "sock5",
-      category: "BAG",
-      name: "Vss",
-      price: "BR6,882.00",
-      deliveryTime: "DELIVERY WITHIN 4 WEEKS",
-    },
-    {
-      brand: "sock6",
-      category: "BAG",
-      name: "Vss",
-      price: "BR6,882.00",
-      deliveryTime: "DELIVERY WITHIN 4 WEEKS",
-    },
-    {
-      brand: "sock6",
-      category: "BAG",
-      name: "Vss",
-      price: "BR6,882.00",
-      deliveryTime: "DELIVERY WITHIN 4 WEEKS",
-    },
-    {
-      brand: "sock6",
-      category: "BAG",
-      name: "Vss",
-      price: "BR6,882.00",
-      deliveryTime: "DELIVERY WITHIN 4 WEEKS",
-    },
-  ];
+  const handleToggleMode = () => {
+    console.log(currentDetailsIndex);
+    setDetails((prevDetails) => {
+      const updatedDetails = [...prevDetails];
+      updatedDetails[currentDetailsIndex] = {
+        ...updatedDetails[currentDetailsIndex],
+        iswhite: !updatedDetails[currentDetailsIndex].iswhite, // Toggle iswhite property
+      };
+      return updatedDetails;
+    });
+  };
 
   const detailsRef = useRef(null);
 
@@ -96,19 +92,7 @@ function App() {
         onEnterBack: () => setCurrentDetailsIndex(index),
       });
     });
-    // panels.forEach((panel, index) => {
-    //   ScrollTrigger.create({
-    //     trigger: panel,
-    //     start: "top top", // Align the start of the panel with the top of the viewport
-    //     end: () => `+=${panel.offsetHeight}`, // Set the end based on panel height
-    //     snap: {
-    //       snapTo: 1, // Snap to one panel at a time
-    //       duration: { min: 0.1, max: 0.2 }, // Set minimum and maximum duration for snapping
-    //       delay: 0.1, // Delay before snapping
-    //       // ease: "power1.inOut", // Easing function for snapping animation
-    //     },
-    //   });
-    // });
+
     const timer = setTimeout(() => {
       setLoading(false); // Set isLoading to false after 5 seconds
     }, 4000);
@@ -129,12 +113,12 @@ function App() {
 
   return (
     <div>
-      {isLoading ? (
+      {/* {isLoading ? (
         <div style={{ width: "100vw", height: "100vh" }}>
           <img src={gif} alt="Loading..." />
           <a href="#">SPLY</a>
         </div>
-      ) : null}
+      ) : null} */}
       <div className="details detailed" ref={detailsRef}>
         <div>
           <a href="#">{details[currentDetailsIndex].brand}</a>
@@ -150,25 +134,19 @@ function App() {
       <div className="panel">
         <SockCanvas />
       </div>
-      <div className="panel">
-        <WhitePant />
-      </div>
-      <div className="panel">
-        <ComputersCanvas />
+      <div className="panel" onClick={handleToggleMode}>
+        {details[1].iswhite ? <ComputersCanvas /> : <WhitePant />}
       </div>
 
-      <div className="panel">
-        <SleeveCanvas />
+      {/* current index xolor checked */}
+      <div className="panel" onClick={handleToggleMode}>
+        {details[2].iswhite ? <WhiteSleeve /> : <SleeveCanvas />}
       </div>
-      <div className="panel">
-        <WhiteSleeve />
+
+      <div className="panel" onClick={handleToggleMode}>
+        {details[3].iswhite ? <WhiteShort /> : <ShortCanvas />}
       </div>
-      <div className="panel">
-        <ShortCanvas />
-      </div>
-      <div className="panel">
-        <WhiteShort />
-      </div>
+
       <div className="panel">
         <ShirtCanvas />
       </div>
